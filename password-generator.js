962152d1f19e0909be6b418842514c46a9cf6930
Ana Marr <acarolinasim@gmail.com>
2020-03-07 20:23:37 +1030

    java code and styling

--- password-generator.js
+++ password-generator.js
@@ -1,11 +1,10 @@
+// Assignment Code
 
-var characterAmountLength = document.getElementById("characterAmountLength")
-var characterAmountNumber = document.getElementById("characterAmountNumber")
-var form = document.getElementById("passwordGeneratorForm")
-var includeUppercaseEl = document.getElementById("includeUppercase")
-var includeNumbersEl = document.getElementById("includeNumbers")
-var includeSymbolsEl = document.getElementById("includeSymbols")
-var passwordDisplay = document.getElementById("password-display")
+var characterAmountEl;
+var includeUppercaseEl;
+var includeNumbersEl;
+var includeSymbolsEl;
+var passwordDisplay = document.getElementById("password")
 
 var Uppercase_char_codes = arrayFromLowToHigh(65, 90);
 var Lowercase_char_codes = arrayFromLowToHigh(97,122);
@@ -13,67 +12,93 @@ var Numbers_char_codes = arrayFromLowToHigh(48,57);
 var Symbols_char_codes = arrayFromLowToHigh(33,47).concat(arrayFromLowToHigh(58,64)).concat(
 arrayFromLowToHigh(91,96));
 
-characterAmountNumber.addEventListener("input", synccharacterAmount)
-characterAmountLength.addEventListener("input", synccharacterAmount)
+document.getElementById("generate").onclick = function(){
 
-//Everytime I try to submit the form, this function bellow will be called
-
-form.addEventListener("submit", e => {
-e.preventDefault() //stop form from refreshing the page after submiting
-
-
-var characterAmount = characterAmountNumber.value
-var includeUppercase = includeUppercaseEl.checked //tells true or false if the box is checked
-var includeNumbers = includeNumbersEl.checked
-var includeSymbols = includeSymbolsEl.checked
-var password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
-passwordDisplay.innerText = password;
-
-})
-
-function generatePassword (characterAmount, includeUppercase, includeNumbers, includeSymbols) {
-
-//Default to lowercase character codes
-let charCodes = Lowercase_char_codes
-if (includeUppercase) charCodes = charCodes.concat(Uppercase_char_codes);
-if (includeNumbers) charCodes = charCodes.concat(Numbers_char_codes);
-if (includeSymbols) charCodes = charCodes.concat(Symbols_char_codes);
-
-var passwordCharacters = []
-for( let i = 0; i < characterAmount; i++){
-    var character = charCodes[Math.floor(Math.random() * charCodes.length)]
-    passwordCharacters.push(String.fromCharCode(character))
-}
-
-return passwordCharacters.join('');
-
-}
-
-function arrayFromLowToHigh(low, high){
-
-var array = []
-for ( let i = low; i<= high; i++){
-
-array.push(i)
-
-}
-
-return array
+  Explanation = continueOrnot();
+  characterAmountEl = prompt("Choose your password length: min 8 and max 128 characters")
+  includeUppercaseEl = confirm("Do you wish to add uppercase characters?")
+  includeNumbersEl = confirm("Do you wish to add numbers?")
+  includeSymbolsEl = confirm("Do you wish to add symbols?")
 
 
-}
+  var password = generatePassword(characterAmountEl, includeUppercaseEl, includeNumbersEl, includeSymbolsEl)
+  passwordDisplay.innerText = password;
+  
+  }
+
+  function continueOrnot(event){
+  
+    Explanation = confirm("Hi! To include a character type click 'Ok' or 'Cancel' to no include it")
+
+    if (Explanation === true){
+      
+      return true;
+    }
+    else{
+    
+      event.preventDefault();
+     
+    }
+    
+  }
+
+  function generatePassword (characterAmountEl, includeUppercaseEl, includeNumbersEl, includeSymbolsEl) {
+
+    //Default to lowercase character codes
+    let charCodes = Lowercase_char_codes
+   
+    if (includeUppercaseEl) charCodes = charCodes.concat(Uppercase_char_codes)
+    if (includeNumbersEl) charCodes = charCodes.concat(Numbers_char_codes);
+    if (includeSymbolsEl) charCodes = charCodes.concat(Symbols_char_codes);
+    
+    var passwordCharacters = []
+    for( let i = 0; i < characterAmountEl; i++){
+        var character = charCodes[Math.floor(Math.random() * charCodes.length)]
+        passwordCharacters.push(String.fromCharCode(character))
+    }
+    
+    return passwordCharacters.join('');
+    
+    }
+
+    function arrayFromLowToHigh(low, high){
+
+      var array = []
+      for ( let i = low; i<= high; i++){
+      
+      array.push(i)
+      
+      }
+      
+      return array
+      
+      
+      }
 
 
-//When I scroll the range bar the numbers will change accondingly 
 
-function synccharacterAmount(e) {
 
-var value = e.target.value
 
-characterAmountNumber.value = value
-characterAmountLength.value = value
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
 
 
 
